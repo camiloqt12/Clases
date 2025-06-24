99b78ee0e191cc9dfbf0d2eb06efaa6471e308f6
camiloqt12 <camiloqt12@gmail.com>
2025-06-24 18:39:16 -0500

    nueva documentacion

--- Leccion.py
+++ Leccion.py
@@ -1,3 +1,10 @@
-print("hola me llamo Camilo.")
-print("Nicolas mama culo y se comio a camila" \
-"")
+#ls -a es para saber donde estoy
+#cd .. para salir, cd "nombre de carpeta" para entrar
+#git init es para arrancar el repositorio
+#git commit -m "para guardar cambios"
+#git remote add origin "url del repositorio" para conectar el repositorio local con el remoto
+#git remote -v para ver los repositorios remotos
+#git checkout para saber donde estoy
+#git push origin master para subir los cambios al repositorio remoto
+#git pull origin master para bajar los cambios del repositorio remoto
+print("hola me llamo Camilo.") 
